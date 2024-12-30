c564e5cb768c758e404f43d09a7112f1e1dab0ff
AkhilSharma90 <akhilsails@gmail.com>
2024-12-30 12:20:15 +0530

    added examples for methods

---
+++ small_examples/methods/example.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+// Method for the Person type
+func (p Person) Greet() {
+	fmt.Printf("Hello, my name is %s, and I am %d years old.\n", p.Name, p.Age)
+}
+func main() {
+	alice := Person{Name: "Alice", Age: 30}
+	alice.Greet() // Call the method
+}
