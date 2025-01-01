b8e111bc594a1841a2fc527e9869347ed9fea94f
AkhilSharma90 <akhilsails@gmail.com>
2025-01-01 10:39:57 +0530

    changes in closure recursion

--- small_examples/closure_recursion/sum_of_array.go
+++ small_examples/closure_recursion/sum_of_array.go
@@ -13,3 +13,15 @@ func main() {
 	nums := []int{1, 2, 3, 4, 5}
 	fmt.Println("Sum of array:", sum(nums, len(nums))) // Output: 15
 }
+
+// sum(nums, 5) → nums[4] (5) + sum(nums, 4)
+// sum(nums, 4) → nums[3] (4) + sum(nums, 3)
+// sum(nums, 3) → nums[2] (3) + sum(nums, 2)
+// sum(nums, 2) → nums[1] (2) + sum(nums, 1)
+// sum(nums, 1) → nums[0] (1) + sum(nums, 0)
+
+// sum(nums, 1) = 1
+// sum(nums, 2) = 2 + 1 = 3
+// sum(nums, 3) = 3 + 3 = 6
+// sum(nums, 4) = 4 + 6 = 10
+// sum(nums, 5) = 5 + 10 = 15
